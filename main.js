14ecf6d9f0cbaf80f471dece5e2d3d04fda8bf31
Brandi Anderson <branderso87@gmail.com>
2017-06-15 13:28:26 -0500

    fix i not an element and push.textContent

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-const numbers = document.querySelectorAll(".numbers")
-let input = [];
+const keys = document.querySelectorAll(".keys")
+const theDisplay = document.querySelector(".display")
+var input = []
+let answer = ''
 
-for(i = 0; i < numbers.length; i++){
-  numbers[i].addEventListener("click", addNumbers());
-
-  function addNumbers(){
-    input.push(numbers[i].id); console.log(input)
-  }
-
-// rewrite index.html so that all input keys have class = keys so we can consistently loop through keys.length.
-// 
+for(i = 0; i < keys.length; i++){
+  keys[i].addEventListener('click', function () {
+    switch(i){
+      case 0: // clear
+        console.log([0]);
+            let input = []
+            theDisplay.innerHTML = ''
+            break
+      case 12:// equals
+          let answer = eval(input.join)
+          theDisplay.textContent = answer
+          break
+      case 13: case 15: case 16:
+          input.push(keys[i].innerHTML)
+          break
+      case 14:// multiply
+          input.push("*")
+          break
+      default:// all other numbers
+          input.push(keys[i].textContent)
+          theDisplay.insertAdjacentHTML('beforeend', keys[i].textContent)
+}
+  });
 }
