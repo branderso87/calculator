91dd548cf412594d4a41d72dec62a19f64d3625a
Brandi Anderson <branderso87@gmail.com>
2017-06-15 14:25:00 -0500

    input keys and display works

--- main.js
+++ main.js
@@ -4,26 +4,28 @@ var input = []
 let answer = ''
 
 for(i = 0; i < keys.length; i++){
-  keys[i].addEventListener('click', function () {
-    switch(i){
-      case 0: // clear
-        console.log([0]);
-            let input = []
-            theDisplay.innerHTML = ''
-            break
-      case 12:// equals
-          let answer = eval(input.join)
-          theDisplay.textContent = answer
-          break
-      case 13: case 15: case 16:
-          input.push(keys[i].innerHTML)
-          break
-      case 14:// multiply
-          input.push("*")
-          break
+  keys[i].addEventListener('click', function (event) {
+    const elementClicked = event.target;
+    const elementID = elementClicked.id;
+
+    switch(elementID){
+      case "clear": // clear
+        input = []
+        theDisplay.innerHTML = ''
+        break
+      case "equals":// equals
+        let answer = eval(input.join)
+        theDisplay.textContent = answer
+        break
+      case "/": case "-": case "+":
+        input.push(elementID)
+        break
+      case "*":// multiply
+        input.push("*")
+        break
       default:// all other numbers
-          input.push(keys[i].textContent)
-          theDisplay.insertAdjacentHTML('beforeend', keys[i].textContent)
+        input.push(parseInt(elementID) )
+        theDisplay.insertAdjacentHTML('beforeend', elementID)
 }
   });
 }
