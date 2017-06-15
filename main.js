10e0db36c605c866cf83ef755a35070e4fba88ff
Brandi Anderson <branderso87@gmail.com>
2017-06-15 15:49:08 -0500

    basic calculator functionality complete

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-const keys = document.querySelectorAll(".keys")
-const theDisplay = document.querySelector(".display")
+const keys = document.querySelectorAll('.keys')
+const theDisplay = document.querySelector('.display')
 var input = []
 let answer = ''
 
-for(i = 0; i < keys.length; i++){
+for(i = 0; i < keys.length; i++) {
   keys[i].addEventListener('click', function (event) {
-    const elementClicked = event.target;
-    const elementID = elementClicked.id;
+    const elementClicked = event.target
+    const elementID = elementClicked.id
 
-    switch(elementID){
-      case "clear": // clear
+
+    switch(elementID) {
+      case 'clear': // clear
         input = []
         theDisplay.innerHTML = ''
         break
-      case "equals":// equals
-        let answer = eval(input.join)
+      case 'equals':// equals
+        let answer = eval(input.join(""))//"" gets rid of the commas in the string
         theDisplay.textContent = answer
+        console.log(answer)
         break
-      case "/": case "-": case "+":
+      case '/': case '-': case '+':
         input.push(elementID)
         break
-      case "*":// multiply
-        input.push("*")
+      case '*':// multiply
+        input.push('*')
         break
       default:// all other numbers
-        input.push(elementID)
+        input.push(parseInt(elementID))
         theDisplay.textContent = elementID
     }
+    console.log(input)
   })
 }
